1162f756de0b9edcd18356edc4c46d01666f11b9
第一システム開発部 佐々木　正太郎 <shotaro-sasaki@dac.co.jp>
2017-12-08 18:21:03 +0900

    rename

--- interfaces/customhandler.go
+++ interfaces/customhandler.go
@@ -9,14 +9,14 @@ import (
 	"github.com/shotarosasaki/publisher/infrastructure/messaging"
 )
 
-// TODO リクエストパーサー、バリデーター、パブリッシャー、TDロガー等をfunc型として定義し、wrapHandlerの引数に追加！
-type custumiser interface {
+// TODO リクエストパーサー、バリデーター、パブリッシャー、TDロガー等をfunc型として定義し、CreateHandlerFuncの引数に追加！
+type customHandler interface {
 	// TODO I/F検討
 	// TODO バリデーションを含めるかメソッド分けるか検討
 	ParseRequest(r *http.Request)
 }
 
-func CreateHandlerFunc(c custumiser, cfg *config.Config) func(http.ResponseWriter, *http.Request) {
+func CreateHandlerFunc(c customHandler, cfg *config.Config) func(http.ResponseWriter, *http.Request) {
 	pub, err := messaging.NewPublisher(cfg.Queue)
 	if err != nil {
 		// TODO エラーログ
@@ -28,6 +28,9 @@ func CreateHandlerFunc(c custumiser, cfg *config.Config) func(http.ResponseWrite
 
 		// TODO リクエストパーサーのI/F見直し！（パース後の型を定義して、その結果に対して後続処理！）
 		c.ParseRequest(r)
+
+		// TODO パース・バリデーションまで終わったらその時点でレスポンス返すようにする！
+		// TODO 以降のCloudPubSubへの送信はゴルーチン内で行う！
 
 		// TODO とりあえずべたで
 		data := []byte("Hello world !!!")
